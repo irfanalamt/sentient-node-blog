2c7b89e86ffd5624caf22d7f588ff127d7e2686e
Irfan <irfanalamt@gmail.com>
2022-08-30 12:01:23 -0400

    Test: trying card animations

--- components/Post.js
+++ components/Post.js
@@ -10,65 +10,84 @@ import {
   CardActions,
 } from '@mui/material';
 import StickyNote2RoundedIcon from '@mui/icons-material/StickyNote2Rounded';
+import { useInView, motion } from 'framer-motion';
+import { useRef } from 'react';
 
 const Post = ({ post, handleClick }) => {
+  const inViewRef = useRef(null);
+  const isInView = useInView(inViewRef);
+  const variants = {
+    active: {
+      scale: [0.95, 1],
+      opacity: 1,
+      transition: { duration: 0.4 },
+    },
+    inactive: {
+      scale: [1, 0.95],
+      opacity: 0,
+      transition: { duration: 0.2, delay: 0.2 },
+    },
+  };
   return (
-    <Card
-      sx={{
-        maxWidth: 350,
-        mx: 'auto',
-        my: 2,
-        py: 1,
-        backgroundColor: '#fafafa',
-      }}
-    >
-      <CardHeader
-        disableTypography={true}
-        title={
-          <Typography
-            sx={{
-              fontSize: '1.2rem',
-              fontWeight: 'bold',
-              fontFamily: 'sans-serif',
-              width: 'max-content',
-              borderRadius: 1,
-              px: 1,
-              backgroundColor: '#F8A055',
-            }}
-            gutterBottom
-            variant='h6'
-          >
-            {post.frontMatter.title}
+    <motion.div animate={isInView ? 'active' : 'inactive'} variants={variants}>
+      <Card
+        sx={{
+          maxWidth: 350,
+          mx: 'auto',
+          my: 2,
+          py: 1,
+          backgroundColor: '#fafafa',
+        }}
+      >
+        <CardHeader
+          disableTypography={true}
+          title={
+            <Typography
+              sx={{
+                fontSize: '1.2rem',
+                fontWeight: 'bold',
+                fontFamily: 'sans-serif',
+                width: 'max-content',
+                borderRadius: 1,
+                px: 1,
+                backgroundColor: '#F8A055',
+              }}
+              gutterBottom
+              variant='h6'
+              ref={inViewRef}
+            >
+              {post.frontMatter.title}
+            </Typography>
+          }
+        ></CardHeader>
+        <CardMedia
+          sx={{ boxShadow: 1 }}
+          component='img'
+          height='150'
+          image={post.frontMatter.cover_image}
+          alt=''
+        />
+        <CardContent>
+          <Typography variant='body2'>
+            <StickyNote2RoundedIcon sx={{ fontSize: 15 }} />{' '}
+            {post.frontMatter.excerpt}
           </Typography>
-        }
-      ></CardHeader>
-      <CardMedia
-        sx={{ boxShadow: 1 }}
-        component='img'
-        height='150'
-        image={post.frontMatter.cover_image}
-        alt=''
-      />
-      <CardContent>
-        <Typography variant='body2'>
-          <StickyNote2RoundedIcon sx={{ fontSize: 15 }} />{' '}
-          {post.frontMatter.excerpt}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Link href={`/blog/${post.slug}`}>
-          <Button
-            sx={{ color: '#757575', mx: 1 }}
-            color='secondary'
-            variant='contained'
-            size='small'
-            onClick={handleClick}
-          >
-            Read more
-          </Button>
-        </Link>
-      </CardActions>
-    </Card>
+        </CardContent>
+        <CardActions>
+          <Link href={`/blog/${post.slug}`}>
+            <Button
+              sx={{ color: '#757575', mx: 1 }}
+              color='secondary'
+              variant='contained'
+              size='small'
+              onClick={handleClick}
+            >
+              Read more
+            </Button>
+          </Link>
+        </CardActions>
+      </Card>
+    </motion.div>
   );
 };
 
