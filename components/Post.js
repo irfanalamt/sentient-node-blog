42f4e584ceca7db28c82021381757fb85dc584b1
Irfan <irfanalamt@gmail.com>
2022-08-30 11:11:07 -0400

    Fix: reset to pre-error state

--- components/Post.js
+++ components/Post.js
@@ -10,85 +10,65 @@ import {
   CardActions,
 } from '@mui/material';
 import StickyNote2RoundedIcon from '@mui/icons-material/StickyNote2Rounded';
-import { motion, useInView } from 'framer-motion';
-import { useRef } from 'react';
 
 const Post = ({ post, handleClick }) => {
-  const inViewRef = useRef(null);
-  const isInView = useInView(inViewRef);
-  const variants = {
-    inactive: {
-      y: ['0%', '50%'],
-      opacity: [1, 0.1],
-      transition: { duration: 1, delay: 0.3 },
-    },
-    active: {
-      y: ['50%', '0%'],
-      opacity: [0.1, 1],
-      scale: [0.9, 1],
-      transition: { duration: 0.4, delay: 0.2, ease: 'easeOut' },
-    },
-  };
   return (
-    <motion.span animate={isInView ? 'active' : 'inactive'} variants={variants}>
-      <Card
-        sx={{
-          maxWidth: 350,
-          mx: 'auto',
-          my: 2,
-          py: 1,
-          backgroundColor: '#fafafa',
-        }}
-      >
-        {isInView && <p>card in view</p>}
-        <CardHeader
-          disableTypography={true}
-          title={
-            <Typography
-              sx={{
-                fontSize: '1.2rem',
-                fontWeight: 'bold',
-                fontFamily: 'sans-serif',
-                width: 'max-content',
-                borderRadius: 1,
-                px: 1,
-                backgroundColor: '#F8A055',
-              }}
-              gutterBottom
-              variant='h6'
-            >
-              {post.frontMatter.title}
-            </Typography>
-          }
-        ></CardHeader>
-        <CardMedia
-          sx={{ boxShadow: 1 }}
-          component='img'
-          height='150'
-          image={post.frontMatter.cover_image}
-          alt=''
-        />
-        <CardContent>
-          <Typography variant='body2'>
-            <StickyNote2RoundedIcon sx={{ fontSize: 15 }} />{' '}
-            {post.frontMatter.excerpt}
+    <Card
+      sx={{
+        maxWidth: 350,
+        mx: 'auto',
+        my: 2,
+        py: 1,
+        backgroundColor: '#fafafa',
+      }}
+    >
+      <CardHeader
+        disableTypography={true}
+        title={
+          <Typography
+            sx={{
+              fontSize: '1.2rem',
+              fontWeight: 'bold',
+              fontFamily: 'sans-serif',
+              width: 'max-content',
+              borderRadius: 1,
+              px: 1,
+              backgroundColor: '#F8A055',
+            }}
+            gutterBottom
+            variant='h6'
+          >
+            {post.frontMatter.title}
           </Typography>
-        </CardContent>
-        <CardActions ref={inViewRef}>
-          <Link href={`/blog/${post.slug}`}>
-            <Button
-              sx={{ color: '#757575', mx: 1 }}
-              color='secondary'
-              variant='contained'
-              size='small'
-              onClick={handleClick}
-            >
-              Read more
-            </Button>
-          </Link>
-        </CardActions>
-      </Card>
-    </motion.span>
+        }
+      ></CardHeader>
+      <CardMedia
+        sx={{ boxShadow: 1 }}
+        component='img'
+        height='150'
+        image={post.frontMatter.cover_image}
+        alt=''
+      />
+      <CardContent>
+        <Typography variant='body2'>
+          <StickyNote2RoundedIcon sx={{ fontSize: 15 }} />{' '}
+          {post.frontMatter.excerpt}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Link href={`/blog/${post.slug}`}>
+          <Button
+            sx={{ color: '#757575', mx: 1 }}
+            color='secondary'
+            variant='contained'
+            size='small'
+            onClick={handleClick}
+          >
+            Read more
+          </Button>
+        </Link>
+      </CardActions>
+    </Card>
   );
 };
 
