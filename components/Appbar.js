7a4e28dea503a6e459be734604467e73853e79fa
Irfan <irfanalamt@gmail.com>
2022-09-07 00:03:05 -0400

    Feat: remove rotate animation; change iconColor timeout

--- components/Appbar.js
+++ components/Appbar.js
@@ -3,36 +3,36 @@ import Face6RoundedIcon from '@mui/icons-material/Face6Rounded';
 import { useEffect, useState } from 'react';
 
 const Appbar = () => {
+  const [iconColor, setIconColor] = useState('black');
   const [rotationDeg, setRotationDeg] = useState(0);
-  let deg = 1;
+  let index = 0;
 
   useEffect(() => {
     setInterval(() => {
       console.log('haha✨');
-      changeRotation();
-    }, 100);
+      changeColor();
+    }, 4000);
   }, []);
 
-  function changeRotation() {
-    setRotationDeg(deg);
-    if (deg < 360) {
-      deg += 2;
+  function changeColor() {
+    const colorList = ['#d81b60', '#43a047', 'black'];
+    setIconColor(colorList[index]);
+    if (index < colorList.length - 1) {
+      index++;
       return;
     }
-    deg = 0;
+    index = 0;
   }
-  function setColor() {
-    if (rotationDeg >= 0 && rotationDeg <= 90) return 'black';
-    if (rotationDeg > 90 && rotationDeg <= 180) return '#f9a825';
-    if (rotationDeg > 180 && rotationDeg <= 270) return '#ff8f00';
-    return '#ef6c00';
+
+  function setFontSize() {
+    if (iconColor == '#43a047') return 19;
+    if (iconColor == '#d81b60') return 20;
+    return 18;
   }
 
   return (
     <AppBar sx={{ mb: 1, backgroundColor: '#4897d8' }} position='static'>
-      <Container
-        sx={{ display: 'flex', alignItems: 'center', position: 'relative' }}
-      >
+      <Container sx={{ display: 'flex', alignItems: 'center' }}>
         <Tooltip title=''>
           <Typography
             variant='h6'
@@ -53,13 +53,11 @@ const Appbar = () => {
             Sentient Node
             <Face6RoundedIcon
               sx={{
-                color: setColor(),
-                position: 'absolute',
+                color: iconColor,
                 backgroundColor: '#efebe9',
                 boxShadow: 1,
                 borderRadius: 5,
-                fontSize: 18,
-                rotate: `${rotationDeg}deg`,
+                fontSize: setFontSize(),
               }}
             />
           </Typography>
