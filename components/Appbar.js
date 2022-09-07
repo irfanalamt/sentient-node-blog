48338a815c8ea2a40dbc11c29d4a664e2be713c0
Irfan <irfanalamt@gmail.com>
2022-09-07 11:22:11 -0400

    Chore: remove icon color,size change

--- components/Appbar.js
+++ components/Appbar.js
@@ -3,33 +3,6 @@ import Face6RoundedIcon from '@mui/icons-material/Face6Rounded';
 import { useEffect, useState } from 'react';
 
 const Appbar = () => {
-  const [iconColor, setIconColor] = useState('black');
-  const [rotationDeg, setRotationDeg] = useState(0);
-  let index = 0;
-
-  useEffect(() => {
-    setInterval(() => {
-      console.log('haha✨');
-      changeColor();
-    }, 4000);
-  }, []);
-
-  function changeColor() {
-    const colorList = ['#d81b60', '#43a047', 'black'];
-    setIconColor(colorList[index]);
-    if (index < colorList.length - 1) {
-      index++;
-      return;
-    }
-    index = 0;
-  }
-
-  function setFontSize() {
-    if (iconColor == '#43a047') return 19;
-    if (iconColor == '#d81b60') return 20;
-    return 18;
-  }
-
   return (
     <AppBar sx={{ mb: 1, backgroundColor: '#4897d8' }} position='static'>
       <Container sx={{ display: 'flex', alignItems: 'center' }}>
@@ -53,11 +26,11 @@ const Appbar = () => {
             Sentient Node
             <Face6RoundedIcon
               sx={{
-                color: iconColor,
+                color: 'black',
                 backgroundColor: '#efebe9',
                 boxShadow: 1,
                 borderRadius: 5,
-                fontSize: setFontSize(),
+                fontSize: 18,
               }}
             />
           </Typography>
