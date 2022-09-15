5b8266aed25c116fcc819e6a4867d9c1bcaf8340
Irfan <irfanalamt@gmail.com>
2022-09-14 22:43:28 -0400

    Style: increase fontSize a little

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -37,7 +37,7 @@ const PostPage = ({ frontMatter: { title, cover_image }, slug, content }) => {
       </Typography>
 
       <Paper
-        sx={{ pl: 1, pr: 3, py: 1, my: 1, fontSize: '1rem' }}
+        sx={{ pl: 1, pr: 3, py: 1, my: 1, fontSize: '1.01rem' }}
         dangerouslySetInnerHTML={{ __html: parsedContent }}
       ></Paper>
       <Box
