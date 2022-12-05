20d3df64ff3b8fd976483c4b70060a465a3e49b6
Irfan <irfanalamt@gmail.com>
2022-12-05 18:49:29 -0500

    Refactor: optimize imports, unused code

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,39 +1,32 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import { marked } from 'marked';
-import Link from 'next/link';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import BlurOnIcon from '@mui/icons-material/BlurOn';
+import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded';
+import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
 import {
   Alert,
-  Avatar,
   Box,
   Button,
   Container,
   Fab,
-  Paper,
   Snackbar,
   Typography,
 } from '@mui/material';
-import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
-import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import BlurOnIcon from '@mui/icons-material/BlurOn';
-import { useRef, useState } from 'react';
 import Slide from '@mui/material/Slide';
-import Head from 'next/head';
+import fs from 'fs';
+import matter from 'gray-matter';
+import { marked } from 'marked';
+import Link from 'next/link';
+import path from 'path';
+import { useState } from 'react';
 
-const PostPage = ({ frontMatter: { title, cover_image }, slug, content }) => {
+const PostPage = ({ frontMatter: { title }, slug, content }) => {
   const [ifLiked, setIfLiked] = useState(false);
   const [open, setOpen] = useState(false);
   const [toast, setToast] = useState('');
   const parsedContent = marked.parse(content);
   const [transition, setTransition] = useState(undefined);
 
-  const randomNumber = useRef(null);
-
   const handleClose = () => setOpen(false);
-
-  const generateRandomNumber = () => Math.floor(Math.random() * 5);
 
   const toastContent = [
     'YOU are awesome.',
@@ -46,7 +39,6 @@ const PostPage = ({ frontMatter: { title, cover_image }, slug, content }) => {
 
   const generateRandomToast = () => {
     const randomNumber = Math.floor(Math.random() * 5);
-    console.log('randomNumber: ' + randomNumber);
     setToast(toastContent[randomNumber]);
   };
 
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,16 @@
-import {
-  Avatar,
-  Box,
-  Button,
-  Chip,
-  Container,
-  Paper,
-  Typography,
-} from '@mui/material';
-import Head from 'next/head';
-import Image from 'next/image';
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import Post from '../components/Post';
-import Grid from '@mui/material/Unstable_Grid2';
-import { useEffect } from 'react';
-import { motion } from 'framer-motion';
 import Face6RoundedIcon from '@mui/icons-material/Face6Rounded';
-import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
 import ScienceOutlinedIcon from '@mui/icons-material/ScienceOutlined';
+import { Avatar, Box, Paper, Typography } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+import { motion } from 'framer-motion';
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+import { useEffect } from 'react';
+import Post from '../components/Post';
 
-import PsychologyAltIcon from '@mui/icons-material/PsychologyAlt';
-import NaturePeopleOutlinedIcon from '@mui/icons-material/NaturePeopleOutlined';
 import Diversity1OutlinedIcon from '@mui/icons-material/Diversity1Outlined';
+import PsychologyAltIcon from '@mui/icons-material/PsychologyAlt';
 
 export default function Home({ posts }) {
   useEffect(() => {
@@ -46,10 +34,6 @@ export default function Home({ posts }) {
     <>
       <Paper
         sx={{
-          // backgroundImage: `url(/images/walls/1.jpg)`,
-          // backgroundSize: 'cover',
-          // backgroundRepeat: 'no-repeat',
-          // backgroundPosition: 'center',
           backgroundColor: '#2A2922',
           height: 'max-content-height',
           padding: 1,
@@ -137,13 +121,11 @@ export default function Home({ posts }) {
         </motion.div>
       </Paper>
       <Grid sx={{ justifyContent: 'center' }} container>
-        {posts.map((post, i) => {
-          return (
-            <Grid key={i} sm={12} md={5} lg={4}>
-              <Post handleClick={handleClick} key={i} post={post} />
-            </Grid>
-          );
-        })}
+        {posts.map((post, i) => (
+          <Grid key={i} sm={12} md={5} lg={4}>
+            <Post handleClick={handleClick} key={i} post={post} />
+          </Grid>
+        ))}
       </Grid>
     </>
   );
